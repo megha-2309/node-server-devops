3b694c2e9b7f32476ab49ab11081c725d0e9c2b6
megha-2309 <meghabholla2309@gmail.com>
2026-09-08 15:21:32 +0530

    Update

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app= express()
 const PORT  = process.env.PORT ?? 8000
 
 app.get('/',(req,res)=>{
-    res.send({message:"Server is healthy"})
+    res.send({message:"Server is healthy v3.1"})
 })
 
 app.listen(PORT,()=>{
